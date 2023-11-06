7f4b1d5368d9204f15afe352757e40c1b2b355ac
Reed Meher <reed.meher@gmail.com>
2023-11-06 10:46:34 -0600

    refining responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,15 +12,6 @@ const Header = () => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="absolute" style={{ backgroundColor: "black" }}>
         <Toolbar>
-          {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
           <Typography
             variant="h6"
             component="div"
