61dbbeabfb1365c33e086470b08fc4ec591794ac
Reed Meher <reed.meher@gmail.com>
2023-12-07 23:34:43 -0600

    apollo server errors

--- server/server.js
+++ server/server.js
@@ -24,10 +24,6 @@ const startApolloServer = async () => {
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
 
-    app.use('/graphql', expressMiddleware(server, {
-        context: authMiddleware
-    }));
-
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../client/dist')));
 
@@ -35,6 +31,8 @@ const startApolloServer = async () => {
             res.sendFile(path.join(__dirname, '../client/dist/index.html'));
         });
     }
+
+    app.use('/graphql', expressMiddleware(server));
 
     db.once('open', () => {
         app.listen(PORT, () => {
