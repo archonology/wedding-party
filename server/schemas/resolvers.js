f7651842bf1ad711e953a0aabc45ab09b61e7d43
Reed Meher <reed.meher@gmail.com>
2023-12-07 21:15:20 -0600

    resolvers added

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,42 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Admin, User, Topic, Subtopic, Resource, QuickLink, Post } = require('../models');
+
+const { signToken, signAdminToken } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select("-__v -password");
+                return userData;
+            }
+            throw new AuthenticationError("Please log in to continue");
+        },
+    },
+
+    Mutation: {
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        loginUser: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No user found by that email address.');
+            }
+
+            const correctPassword = await user.isCorrectPassword(password);
+
+            if (!correctPassword) {
+                throw new AuthenticationError('Password is incorrect.');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+    }
+}
+
+module.exports = resolvers;
